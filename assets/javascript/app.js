101af93ce9682b1cc31f75f53c221395c8c40e05
Alex Curington <alex.curington@me.com>
2019-01-18 15:41:34 -0600

    no message

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,196 +1,136 @@
-$(document).ready(function () {
-	var options = [
-		{
-			question: "What is the meaning alligators in dreams?", 
-			choice: ["danger", "new romance", "death", "peace" ],
-			answer: 0,
-			photo: "assets/images/danger.gif"
-		 },
-		 {
-			 question: "What does the the color yellow symbolize?", 
-			choice: ["peace", "power", "happiness", "love" ],
-			answer: 2,
-			photo: "assets/images/smile.png"
-		 }, 
-		 {
-			 question: "Dreaming of jellyfish symbolizes...", 
-			choice: ["unsolved mysteries", "broken friendships", "you're pregnant", "painful memories" ],
-			answer: 3,
-			photo: "assets/images/jellyfish.gif"
-		}, 
-		{
-			question: "What does the color black represent in a dream?", 
-			choice: ["cats", "death", "sleepiness", "vitality" ],
-			answer: 1,
-			photo: "assets/images/death.gif"
-		}, 
-		{
-			question: "Dreams containing ladders might represent...", 
-			choice: ["failure in the future", "someone needs you", "chronic illness", "new levels of growth" ],
-			answer: 3,
-			photo: "assets/images/ladder.gif"
-		}, 
-		{
-			question: "Which flower symbolizes innocence?", 
-			choice: ["Daisy", "Carnation", "Delphinium", "Daffodil" ],
-			answer: 0,
-			photo: "assets/images/daisy.gif"
-		}, 
-		{
-			question: "Dreaming that you are blind might indicate...", 
-			choice: ["broken dreams", "desire to escape", "wealth and romance", "impending doom" ],
-			answer: 1,
-			photo: "assets/images/blind.gif"
-		}, 
-		{
-			question: "Which stone symbolizes truth?", 
-			choice: ["Amethyst", "Fancy Diamonds", "Lapis Lazuli", "Tourmaline" ],
-			answer: 2,
-			photo: "assets/images/truth.gif"
-		}];
+
+
+$(document).ready(function(){
+
+//initial array 
+var colors = ["red", "coral", "lime green", "gold", "blue", "honeydew", "heliotrope", "magenta", "cyan", "seaweed", "turquoise", "violet-red", "pink", "yellow", "orchid", "aquamarine", "mint", "beige", "rose", "silver", "mustard", "amethyst", "burgundy", "carrot", "chartreuse", "cinnabar", "grey", "cotton candy", "salmon", "champange", "denim", "indigo", "lavender"]
+GIFArea = " "
 	
-	var correctCount = 0;
-	var wrongCount = 0;
-	var unanswerCount = 0;
-	var timer = 20;
-	var intervalId;
-	var userGuess ="";
-	var running = false;
-	var qCount = options.length;
-	var pick;
-	var index;
-	var newArray = [];
-	var holder = [];
+//button
+
+//function for displaying tv show data
+function renderButtons() {
 	
+//deleting the color buttons prior to adding new color buttons
+$("#colors-view").empty();
 	
+//looping through the array of colors
+for (var i=0; i < colors.length; i++) {
 	
-	$("#reset").hide();
-	//click start button to start game
-	$("#start").on("click", function () {
-			$("#start").hide();
-			displayQuestion();
-			runTimer();
-			for(var i = 0; i < options.length; i++) {
-		holder.push(options[i]);
-	}
-		})
-	//timer start
-	function runTimer(){
-		if (!running) {
-		intervalId = setInterval(decrement, 1000); 
-		running = true;
-		}
-	}
-	//timer countdown
-	function decrement() {
-		$("#timeleft").html("<h3>Time remaining: " + timer + "</h3>");
-		timer --;
+//dynamically generate buttons for each color in the array.
+//this code $("<button>") is all jquery needs to create the start and end tag. (<button></button>)
+var a = $('<button>');
+
+//Adding a class
+a.addClass('color');
 	
-		//stop timer if reach 0
-		if (timer === 0) {
-			unanswerCount++;
-			stop();
-			$("#answerblock").html("<p>Time is up! The correct answer is: " + pick.choice[pick.answer] + "</p>");
-			hidepicture();
-		}	
-	}
+//adding a data-attribute with a value of the television at index i
+a.attr('data-name', colors[i]);
 	
-	//timer stop
-	function stop() {
-		running = false;
-		clearInterval(intervalId);
-	}
-	//randomly pick question in array if not already shown
-	//display question and loop though and display possible answers
-	function displayQuestion() {
-		//generate random index in array
-		index = Math.floor(Math.random()*options.length);
-		pick = options[index];
+//providing the button's text with a value of the color at index i
+a.text(colors[i]);
+
+//adding the button the html
+$("#colors-view").append(a);
+}
+s=
+$("#color-input").focus();
+}
+
+renderButtons();
 	
-	//	if (pick.shown) {
-	//		//recursive to continue to generate new index until one is chosen that has not shown in this game yet
-	//		displayQuestion();
-	//	} else {
-	//		console.log(pick.question);
-			//iterate through answer array and display
-			$("#questionblock").html("<h2>" + pick.question + "</h2>");
-			for(var i = 0; i < pick.choice.length; i++) {
-				var userChoice = $("<div>");
-				userChoice.addClass("answerchoice");
-				userChoice.html(pick.choice[i]);
-				//assign array position to it so can check answer
-				userChoice.attr("data-guessvalue", i);
-				$("#answerblock").append(userChoice);
-	//		}
-	}
+//============CLICK BUTTON/ LISTENERS==================================
+//this function handles events where one button is clicked
+$("#add-color").on('click', function() {
 	
+//event.preventDefault() prevents the form from trying to submit itself
+//we're using a form so that the user can hit enter instread of clicking the button
+event.preventDefault();
 	
+//This line will grab the text from the input box
+var color = $("#color-input").val().trim();
 	
-	//click function to select answer and outcomes
-	$(".answerchoice").on("click", function () {
-		//grab array position from userGuess
-		userGuess = parseInt($(this).attr("data-guessvalue"));
+//this color from the textbox is then added to our array
+colors.push(color);
 	
-		//correct guess or wrong guess outcomes
-		if (userGuess === pick.answer) {
-			stop();
-			correctCount++;
-			userGuess="";
-			$("#answerblock").html("<p>Correct!</p>");
-			hidepicture();
+//calling renderButtons which handles the processing of our color array
+renderButtons();
 	
-		} else {
-			stop();
-			wrongCount++;
-			userGuess="";
-			$("#answerblock").html("<p>Wrong! The correct answer is: " + pick.choice[pick.answer] + "</p>");
-			hidepicture();
-		}
-	})
-	}
+});
 	
+//==============DISPLAY INFO==============================
+$(document).on('click', 'button',  function() {
+
+// Deleting the colors prior to adding new colors
+// (this is necessary otherwise we will have repeat buttons)
+
+$('#GIFArea').empty(); 
+var b = $(this).attr('data-name');		
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + b + "&api_key=dc6zaTOxFJmzC&limit=12";  
+console.log(queryURL); 
 	
-	function hidepicture () {
-		$("#answerblock").append("<img src=" + pick.photo + ">");
-		newArray.push(pick);
-		options.splice(index,1);
+//standard ajax call to get request
+$.ajax({
+url: queryURL,
+method: 'GET'
+})
+
+//after the data comes back from the API
+.done(function(response) {
+console.log(response);
+
+var results = response.data;
+for (var i = 0; i < results.length; i++) {
+
+//creating a div with the class item
+var gifDiv = $('<div class="item">');
+
+//storing the result items rating        
+var rating = results[i].rating;
+
+//creating an element to have the rating displayed
+var r = $('<p>').text();
+
+//creating a image tag
+var gifImage = $('<img>');
+
+//giving the image tag an src attribute of a property pulled off the result item
+gifImage.attr('src', results[i].images.fixed_height_still.url)
+.attr('data-still', results[i].images.fixed_height_still.url)
+.attr('data-animate', results[i].images.fixed_height.url)
+.attr('data-state', "still")
+.addClass("showImage");
+
+//displaying the rating & image
+gifDiv.append(r)
+.append(gifImage);	                    
 	
-		var hidpic = setTimeout(function() {
-			$("#answerblock").empty();
-			timer= 20;
+//prepending data not necessary since cleared             	  
+$('#GIFArea').prepend(gifDiv);
+}
 	
-		//run the score screen if all questions answered
-		if ((wrongCount + correctCount + unanswerCount) === qCount) {
-			$("#questionblock").empty();
-			$("#questionblock").html("<h3>Game Over!  Here's how you did: </h3>");
-			$("#answerblock").append("<h4> Correct: " + correctCount + "</h4>" );
-			$("#answerblock").append("<h4> Incorrect: " + wrongCount + "</h4>" );
-			$("#answerblock").append("<h4> Unanswered: " + unanswerCount + "</h4>" );
-			$("#reset").show();
-			correctCount = 0;
-			wrongCount = 0;
-			unanswerCount = 0;
+});
+});
 	
-		} else {
-			runTimer();
-			displayQuestion();
-	
-		}
-		}, 3000);
-	
-	
-	}
-	
-	$("#reset").on("click", function() {
-		$("#reset").hide();
-		$("#answerblock").empty();
-		$("#questionblock").empty();
-		for(var i = 0; i < holder.length; i++) {
-			options.push(holder[i]);
-		}
-		runTimer();
-		displayQuestion();
-	
-	})
-	
-	})
+//====================Still and Animate Image ==================================
+// Listens for a click on any image (dynamic)
+// $('.showImage').on('click', function(){ --> won't work here
+$(document).on('click', '.showImage',  function() {
+var state = $(this).data('state');
+
+//If the clicked image's state is still, update its src attribute to what its data-animate value is
+if (state == "still") {
+console.log("still image works");
+
+// Then, set the image's data-state to animate
+$(this).attr('src', $(this).data('animate'))
+.data('state', 'animate');
+} else {
+			
+//  else set src to the data-still value
+console.log("animated image works");
+$(this).attr('src', $(this).data('still'))
+.data('state', 'still');               
+}
+});
+});
